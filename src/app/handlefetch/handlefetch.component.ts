955e0dfc5ef3c62ea7856b52cb9c8150585e8f98
Kartik <kartik.arora1214@gmail.com>
2019-10-17 16:53:25 +0530

    remove shift from keyboard cuz Lakshit

--- src/app/handlefetch/handlefetch.component.ts
+++ src/app/handlefetch/handlefetch.component.ts
@@ -43,10 +43,10 @@ export class HandlefetchComponent implements OnInit {
       onKeyPress: button => this.onKeyPress(button),
       layout: {
         default: [
-          "1 2 3 4 5 6 7 8 9 0",
-          "q w e r t y u i o p",
-          "a s d f g h j k l",
-          "{shift} z x c v b n m {backspace}",
+          "1 2 3 4 5 6 7 8 9 0 {backspace}",
+          "Q W E R T Y U I O P",
+          "A S D F G H J K L",
+          "Z X C V B N M . _",
 
         ],
         shift: [
